0c9ea91fd68fc89d7343de0007920f981acf1cc6
Zach Kauffman <zach.kauffman0@gmail.com>
2022-04-02 20:38:40 -0700

    add basic waves

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,11 @@
 // import * as myModule from "./mymodule.js";
 
 let player
+let wave
+let timeSinceStart
+
+let lowtide = false
+let hightide = false
 
 runOnStartup(async runtime =>
 {
@@ -10,6 +15,8 @@ runOnStartup(async runtime =>
 	// Note layouts, objects etc. are not yet available.
 	
 	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
+	
+	timeSinceStart = 0
 });
 
 async function OnBeforeProjectStart(runtime)
@@ -24,9 +31,36 @@ async function OnBeforeProjectStart(runtime)
 	
 	player.y = 160
 	player.x = 160
+	
+	wave = runtime.objects.Wave.getFirstInstance()
+	wave.x = 160
+	wave.y = 640
 }
 
 function Tick(runtime)
 {
+	const dt = runtime.dt;
+	timeSinceStart += dt
+	Wave(timeSinceStart, Math.floor(Math.random() * 4))
+}
+
+function Wave(time, direction) {
+	console.log(direction)
+	if(direction == 0) {
+		wave.x = 0
+		wave.y = 560 - (60 * Math.sin(time))
+	}
+	else if (direction == 1) {
+		wave.x = 0
+		wave.y = -240 + (60 * Math.sin(time))
+	}
+	else if (direction == 2) {
+		wave.x = 560 - (60 * Math.sin(time))
+		wave.y = 0
+	}
+	else if (direction == 3) {
+		wave.x = -240 + (60 * Math.sin(time))
+		wave.y = 0
+	}
 
 }
